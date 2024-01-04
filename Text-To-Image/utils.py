1bd6254cb9f82eb059161a242a136e39df11a28d
Kumar Rohit <93335681+k-Rohit@users.noreply.github.com>
2024-01-04 20:36:09 +0530

    Delete Text-To-Image/utils.py

--- Text-To-Image/utils.py
+++
@@ -1,8 +0,0 @@
-from diffusers import StableDiffusionPipeline
-import torch
-def generate_image_with_diffusers(input_text):
-    pipeline = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
-    pipeline = pipeline.to("cuda")
-    prompt = input_text
-    image = pipeline(prompt).images[0] 
-    return image
